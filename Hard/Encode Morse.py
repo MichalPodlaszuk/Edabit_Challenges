d3d1bc00fee7369269a7b64db496c5b566b4592c
MichalPodlaszuk <75248302+MichalPodlaszuk@users.noreply.github.com>
2024-12-07 22:05:50 +0100

    Encode Morse+
    script to encode a string into morse alphabet with additional function to decode it

---
+++ Hard/Encode Morse.py
@@ -0,0 +1,63 @@
+test_str = "EDABBIT CHALLENGE"
+
+
+def encode_morse(str):
+    char_to_dots = {
+        'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.',
+        'G': '--.', 'H': '....', 'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..',
+        'M': '--', 'N': '-.', 'O': '---', 'P': '.--.', 'Q': '--.-', 'R': '.-.',
+        'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-',
+        'Y': '-.--', 'Z': '--..', ' ': ' ', '0': '-----',
+        '1': '.----', '2': '..---', '3': '...--', '4': '....-', '5': '.....',
+        '6': '-....', '7': '--...', '8': '---..', '9': '----.',
+        '&': '.-...', "'": '.----.', '@': '.--.-.', ')': '-.--.-', '(': '-.--.',
+        ':': '---...', ',': '--..--', '=': '-...-', '!': '-.-.--', '.': '.-.-.-',
+        '-': '-....-', '+': '.-.-.', '"': '.-..-.', '?': '..--..', '/': '-..-.'
+    }
+    out_list = []
+    for char in str:
+        out_list.append(char_to_dots[char])
+    out_str = ' '.join(out_list)
+    return out_str
+
+
+encoded = encode_morse(test_str)
+
+
+def decode_morse(strn):
+    #taking original dictionary provided by edabit
+    char_to_dots = {
+        'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.',
+        'G': '--.', 'H': '....', 'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..',
+        'M': '--', 'N': '-.', 'O': '---', 'P': '.--.', 'Q': '--.-', 'R': '.-.',
+        'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-',
+        'Y': '-.--', 'Z': '--..', ' ': ' ', '0': '-----',
+        '1': '.----', '2': '..---', '3': '...--', '4': '....-', '5': '.....',
+        '6': '-....', '7': '--...', '8': '---..', '9': '----.',
+        '&': '.-...', "'": '.----.', '@': '.--.-.', ')': '-.--.-', '(': '-.--.',
+        ':': '---...', ',': '--..--', '=': '-...-', '!': '-.-.--', '.': '.-.-.-',
+        '-': '-....-', '+': '.-.-.', '"': '.-..-.', '?': '..--..', '/': '-..-.'
+    }
+    #inverting the dictionary
+    dots_to_char = {}
+    for key in char_to_dots.keys():
+        dots_to_char[char_to_dots[key]] = key
+    #splitting the encoded string by spaces
+    enc_list = strn.split(" ")
+    #replacing the instances of 2 consecutive blank spaces created by the split operation with a single space
+    while '' in enc_list:
+        enc_list.remove('')
+        enc_list[enc_list.index('')] = ' '
+    #rest of the code is analogical to the above encoder function
+    out_list = []
+    for item in enc_list:
+        out_list.append(dots_to_char[item])
+    out = ''.join(out_list)
+    return out
+
+
+decoded = decode_morse(encoded)
+
+print(encoded)
+print(decoded)
+
